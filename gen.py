319b9f77646b5e36a77b31ce712b93457ea05040
Miha Filej <miha@filej.net>
2016-07-04 15:25:36 +0200

    Allow generation length to mimic distribution

--- gen.py
+++ gen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import pickle, argparse, time
+import pickle, argparse, time, random, sys
 import numpy as np
 from datetime import datetime
 from hmmlearn import hmm
@@ -11,26 +11,41 @@ args = argparse.ArgumentParser(
         description="Generate text with a HMM")
 args.add_argument("-l", "--num-lines", type=int, default=1,
         help="number of lines to generate")
-args.add_argument("-w", "--num-words", type=int, default=25,
-        help="number of words per line")
+
+group = args.add_mutually_exclusive_group(required=True)
+group.add_argument("-w", "--num-words", type=int, default=25,
+        help="number of words per line (excludes -d)")
+group.add_argument("-d", "--num-words-distribution", type=argparse.FileType("r"),
+        help="file containing numbers of words per line (one number per line; alternative to -w)")
+
 args.add_argument("--seed", type=int, default=datetime.now().microsecond,
         help="seed number to configure repeatable random generation")
 args.add_argument("filename", 
         help="input filename without extension (assumes FILENAME.pkl and FILENAME.le)")
 args = args.parse_args()
 
+if args.num_words_distribution:
+    len_dist = [int(line) for line in args.num_words_distribution]
+    def num_words(seed):
+        random.seed(seed)
+        return random.choice(len_dist)
+else:
+    def num_words(seed):
+        return args.num_words
+
+
 with open("{0}.le".format(args.filename), "rb") as f:
     le = pickle.load(f)
-
 model = joblib.load("{0}.pkl".format(args.filename))
 
 seed = args.seed
-
 for _i in range(args.num_lines):
-    symbols, _states = model.sample(args.num_words, random_state=seed)
+    symbols, _states = model.sample(num_words(seed=seed), random_state=seed)
     seed = seed + 1
 
     output = le.inverse_transform(np.squeeze(symbols))
     for word in output:
         print(word, end=" ")
-    print("\n")
+    print()
+
+print("seed={0}".format(args.seed), file=sys.stderr)
